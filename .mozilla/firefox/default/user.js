395048e7832bbbca4f901c3841e1d09cc2bfb592
Ratson <github@ratson.name>
2015-04-08 12:48:59 +0800

    Configure Firefox with better defaults

--- .mozilla/firefox/default/user.js
+++ .mozilla/firefox/default/user.js
@@ -1,3 +1,12 @@
+// Better first time experience for advance user
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.startup.page", 3);
+
+// Better defaults
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("browser.newtabpage.enabled", false);
+
 // Disable update checks
 user_pref("app.update.enabled", false);
 user_pref("browser.search.update", false);
