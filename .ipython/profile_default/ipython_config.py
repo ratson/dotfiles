a27ba3bad105879ce40e2ad5727402f5f4340bb7
Ratson <github@ratson.name>
2015-04-13 00:20:45 +0800

    Configure IPython to import common modules

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,3 +1,13 @@
 c = get_config()
 
+c.InteractiveShellApp.exec_lines = [
+    'from __future__ import division',
+    'import io',
+    'import os',
+    'import time',
+    'import sys',
+    'import re',
+    'from datetime import datetime, date, timedelta',
+]
+
 c.TerminalInteractiveShell.confirm_exit = False
