aaeecb5541671067aa4a2b6bc4f6d04718aae28e
Ratson <github@ratson.name>
2015-04-23 14:36:10 +0800

    Configure cider to install Node packages

---
+++ .cider/install-node-packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+packages=(
+bower
+coffee-script
+coffeelint
+csslint
+hoxy
+jshint
+npm
+npm-check-updates
+)
+
+function install_package {
+  echo "Installing Node package: $1"
+  npm ls -g --depth=0 --parseable $1 > /dev/null 2>&1 || npm install $1 -g
+}
+
+for package in ${packages[@]} ; do
+  install_package $package
+done
