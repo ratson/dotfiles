595618068eb60a463d70f92a2102cfed1eb4878a
Ratson <github@ratson.name>
2015-04-30 05:15:43 +0800

    Add ethan-wspace to Spacemacs

--- .config/spacemacs/personal/packages.el
+++ .config/spacemacs/personal/packages.el
@@ -1,6 +1,7 @@
 (defvar personal-packages
   '(
     clocker
+    ethan-wspace
     w3m
     ))
 
@@ -9,9 +10,16 @@
 
 (defun personal/init-clocker ()
   (use-package clocker
+    :defer t
     :config
     (progn
       (ad-activate 'spacemacs/mode-line-prepare-left))))
+
+(defun personal/init-ethan-wspace ()
+  (use-package ethan-wspace
+    :defer t
+    :init
+    (global-ethan-wspace-mode 1)))
 
 (defun personal/init-w3m ()
   (use-package w3m
