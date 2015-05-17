69c50170f5e12f6a73fd5ef47dfbdf7a1c5b9ea8
Ratson <github@ratson.name>
2015-05-17 10:10:10 +0800

    Add cleanup-osx-junks Zsh alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,6 +4,9 @@ alias et='emacsclient -t'
 # SSH
 alias ssh-unsafe='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 
+# utils
+alias cleanup-osx-junks="find . -type f -name '.DS_Store' -ls -delete"
+
 # shorthand
 alias t=task
 alias va=vagrant
